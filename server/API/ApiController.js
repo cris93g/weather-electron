348e407d49941228db4ae7c88f0435a74748d760
cris93g <37388115+cris93g@users.noreply.github.com>
2019-09-24 11:30:07 -0500

    added last functions to backend

--- server/API/ApiController.js
+++ server/API/ApiController.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const axios = require("axios");
 const { API } = process.env;
-let getLocation = async (req, res) => {
+
+let get5dayCurrentLocation = async (req, res) => {
   let results = await axios.get(
     `http://dataservice.accuweather.com/forecasts/v1/daily/5day/335731?apikey=${API}`
   );
@@ -10,6 +11,39 @@ let getLocation = async (req, res) => {
     res.status(200).json(info);
   }
 };
+
+let getCurrentConditions = async (req, res) => {
+  let results = await axios.get(`
+  http://dataservice.accuweather.com/currentconditions/v1/335731?apikey=${API}
+  `);
+  let info = results.data;
+  if (info) {
+    res.status(200).json(info);
+  }
+};
+
+let getQueryLocation = async (req, res) => {
+  let results = await axios.get(
+    `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=tZjXvNAiH5lTdtS0pSeAd3iGhReU2aFW&q=MESQUITE`
+  );
+  let info = results.data;
+  if (info) {
+    res.status(200).json(info);
+  }
+};
+let getCurrentLocation = async (req, res) => {
+  let results = await axios.get(`
+  http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=tZjXvNAiH5lTdtS0pSeAd3iGhReU2aFW&q=32.807551%2C-96.641733
+  `);
+  let info = results.data;
+  if (info) {
+    res.status(200).json(info);
+  }
+};
+
 module.exports = {
-  getLocation
+  get5dayCurrentLocation,
+  getCurrentConditions,
+  getQueryLocation,
+  getCurrentLocation
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,13 @@
-const { getLocation } = require("../API/ApiController");
+const {
+  get5dayCurrentLocation,
+  getCurrentConditions,
+  getQueryLocation,
+  getCurrentLocation
+} = require("../API/ApiController");
 
 module.exports = app => {
-  app.get(`/api/5day`, getLocation);
+  app.get(`/api/5day`, get5dayCurrentLocation);
+  app.get(`/api/current`, getCurrentConditions);
+  app.get("/api/search", getQueryLocation);
+  app.get("/api/currentLocation", getCurrentLocation);
 };
